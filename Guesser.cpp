5269d595208abf0a84725c5a5945c6132f07fea7
Myles Coleman <myles.coleman@liatrio.com>
2024-05-06 10:48:04 -0700

    finished basic testing

--- Guesser.cpp
+++ Guesser.cpp
@@ -16,37 +16,42 @@ using std::string;
   has 100, the distance is 10.
 */
 unsigned int Guesser::distance(string guess){
-
-  int distance = 0;
-
+  unsigned int distance = 0;
+  
   //if the strings are identical, the distance is 0
-  if (guess == m_secret) { return 0; }
+  if (guess == m_secret) { return distance; }
 
-  //if the strings are the same size, we run a loop
+  //if the strings have the same distance, check if they have the same characters at the same indeces
   if (m_secret.length() == guess.length()) {
     for (int i = 0; i < guess.length(); i++) {
       if (m_secret[i] != guess[i]) {
         distance++;
       }
     }
-  } else {
-    //if there is a > 1 difference in char length between the two, then add it to distance
-    int counter = m_secret.length() - guess.length();
-    distance += abs(counter);
-    
-    
   }
 
-
-  //also need to check if both strings are the same size
-
-  //hello and !hello should return a distance of 5
-  //hello and hello! should return a distance of 1
-  //hello and hello should return a distance of 0
-
-
-
-
+  //if the strings have different distances, add the difference to the distance variable
+  if (m_secret.length() < guess.length()) { //loop using the string with the shortest length
+    if (m_secret.length() != guess.length()) {
+      for (int i = 0; i < m_secret.length(); i++) {
+        if (m_secret[i] != guess[i]) {
+          distance++;
+        }
+      }
+      int num_diff = m_secret.length() - guess.length();
+      distance += abs(num_diff);
+    }
+  } else {
+    if (m_secret.length() != guess.length()) {
+      for (int i = 0; i < guess.length(); i++) {
+        if (m_secret[i] != guess[i]) {
+          distance++;
+        }
+      }
+      int num_diff = m_secret.length() - guess.length();
+      distance += abs(num_diff);
+    }
+  }
   return distance;
 }
 
@@ -57,7 +62,14 @@ unsigned int Guesser::distance(string guess){
   otherwise, it will be truncated at that length.
 */
 Guesser::Guesser(string secret){
+  m_remaining = 3;
 
+  //if the secret is > 32, then truncate it
+  if (secret.length() > 32) {
+    m_secret = secret.substr(0, 32);
+  } else if (secret.length() <= 32) {
+    m_secret = secret;
+  }
 }
 
 /*
@@ -72,6 +84,28 @@ Guesser::Guesser(string secret){
   and the secret.
 */
 bool Guesser::match(string guess){
+
+  //if the secret is locked, return false
+  if (m_remaining == 0) {
+    return false;
+  }
+
+  //if the secret is brute force, return false
+  if (distance(guess) > 2 ) {
+    m_remaining = 0;
+    return false;
+  }
+
+  //if the guess is incorrect decrement by 1 and return false
+  if (distance(guess) == 1) {
+    if (m_remaining > 0) {
+      m_remaining --;
+    }
+    return false;
+  }
+
+  //if the distance is 0, then the guess is correct
+  m_remaining = 3;
   return true;
 }
 
@@ -83,6 +117,6 @@ bool Guesser::match(string guess){
   reset to three (3).
 */
 unsigned int Guesser::remaining(){
-  return 0;
+  return m_remaining;
 }
 
--- GuesserTest.cpp
+++ GuesserTest.cpp
@@ -4,6 +4,7 @@
 
 #include <gtest/gtest.h>
 #include "Guesser.h"
+#include <iostream>
 
 class GuesserTest : public ::testing::Test
 {
@@ -14,9 +15,91 @@ class GuesserTest : public ::testing::Test
 		virtual void TearDown(){} //clean up after each test, (before destructor)
 };
 
-/* Example test
-TEST(GuesserTest, smoke_test)
-{
-    ASSERT_EQ( 1+1, 2 );
+//test correct match
+TEST(GuesserTest, matchCorrect) {
+	Guesser secret("hello");
+	bool actual = secret.match("hello");
+	ASSERT_EQ(true, actual);
 }
-*/
+
+//test distance 1
+TEST(GuesserTest, matchDistanceOne) {
+	Guesser secret("hello");
+	bool actual = secret.match("hello!");
+	ASSERT_EQ(false, actual);
+}
+
+//test brute force 
+TEST(GuesserTest, matchBruteForce) {
+	Guesser secret("hello");
+	secret.match("!hello");
+	bool actual = secret.match("hello");
+	ASSERT_EQ(false, actual);
+}
+
+//test two guesses remaining
+TEST(GuesserTest, matchFailOne) {
+	Guesser secret("hello");
+	secret.match("hello!");
+	bool actual = secret.match("hello");
+	ASSERT_EQ(true, actual);
+}
+
+//test 1 guess remaining
+TEST(GuesserTest, matchFailTwo) {
+	Guesser secret("hello");
+	secret.match("hello!");
+	secret.match("hello!");
+	bool actual = secret.match("hello");
+	ASSERT_EQ(true, actual);
+}
+
+//test 0 guesses remaining
+TEST(GuesserTest, matchFailThree) {
+	Guesser secret("hello");
+	secret.match("hello!");
+	secret.match("hello!");
+	secret.match("hello!");
+	bool actual = secret.match("hello");
+	ASSERT_EQ(false, actual);
+}
+
+//test constructor?
+TEST(GuesserTest, correctConstructor) {
+	Guesser secret("hello");
+	unsigned int actual = secret.remaining();
+	ASSERT_EQ(3, actual);
+}
+
+//test remaining guesses for two fails
+TEST(GuesserTest, remainingResest) {
+	Guesser secret("hello");
+	secret.match("hello!");
+	secret.match("hello!");
+	bool actual = secret.match("hello");
+	ASSERT_EQ(true, actual);
+}
+
+TEST(GuesserTest, remainingTwo) {
+	Guesser secret("hello");
+	secret.match("hello!");
+	unsigned int actual = secret.remaining();
+	ASSERT_EQ(2, actual);
+}
+
+TEST(GuesserTest, remainingOne) {
+	Guesser secret("hello");
+	secret.match("hello!");
+	secret.match("hello!");
+	unsigned int actual = secret.remaining();
+	ASSERT_EQ(1, actual);
+}
+
+TEST(GuesserTest, remainingZero) {
+	Guesser secret("hello");
+	secret.match("hello!");
+	secret.match("hello!");
+	secret.match("hello!");
+	unsigned int actual = secret.remaining();
+	ASSERT_EQ(0, actual);
+}
